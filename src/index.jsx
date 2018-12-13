de3ad6fcb85d4b24d6306ef492f2a3f9fc48b856
Denis <ddujota@internationalsafety.com>
2018-12-13 01:05:50 -0500

    added a loading state and error state

--- src/index.jsx
+++ src/index.jsx
@@ -6,20 +6,31 @@ class App extends Component {
   // Belongs to the JS language itself to initialize a class
   constructor(props) {
     super(props); // a reference to the parent's constructor function so that it runs
-    this.state = { lat: null }; // this is the only time we directly mutate the state instead of setState
+    this.state = { lat: null, errorMessage: '' }; // this is the only time we directly mutate the state instead of setState
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      error => console.log(error)
+      error => {
+        this.setState({ errorMessage: error.message });
+      }
     );
   }
 
   // React says we have to define Render!!!!
   render() {
-    const { lat } = this.state;
-    return <div>Latitude: {lat}</div>;
+    const { lat, errorMessage } = this.state;
+
+    if (errorMessage && !lat) {
+      return <div>Error: {errorMessage}</div>;
+    }
+
+    if (!errorMessage && lat) {
+      return <div> Latitude: {lat} </div>;
+    }
+
+    return <div> Loading....</div>;
   }
 }
 
