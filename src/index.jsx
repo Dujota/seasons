1652e8a522d26e559ed6c44d5e185bdf6ace252b
Denis <ddujota@internationalsafety.com>
2018-12-13 00:09:06 -0500

    refactored the fucntional component into a class and initializing state

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    error => console.log(error)
-  );
+class App extends Component {
+  // Belongs to the JS language itself to initialize a class
+  constructor(props) {
+    super(props); // a reference to the parent's constructor function so that it runs
+  }
 
-  return <div>Hi there!</div>;
-};
+  // React says we have to define Render!!!!
+  render() {
+    this.state = { position: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState(position),
+      error => console.log(error)
+    );
+
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
