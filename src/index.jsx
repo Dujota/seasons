9a2f7d6e784805cf77cce17f9e03abf3d6853149
Denis <ddujota@internationalsafety.com>
2018-12-12 23:45:46 -0500

    creatd the get current position callbak and rendered to index

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
+  );
+
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
