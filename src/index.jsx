5b1903d744a1e5f79558426a2312e4a1260daf07
Denis <ddujota@internationalsafety.com>
2018-12-13 00:45:33 -0500

    comments added

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   // Belongs to the JS language itself to initialize a class
   constructor(props) {
     super(props); // a reference to the parent's constructor function so that it runs
-    this.state = { lat: null };
+    this.state = { lat: null }; // this is the only time we directly mutate the state instead of setState
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
