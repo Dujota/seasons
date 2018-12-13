d0f1db4908f0b4e25aa860f2081f6f6c4c09fcd6
Denis <ddujota@internationalsafety.com>
2018-12-13 00:10:48 -0500

    init lat into state as null thru constructor

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,11 @@ class App extends Component {
   // Belongs to the JS language itself to initialize a class
   constructor(props) {
     super(props); // a reference to the parent's constructor function so that it runs
+    this.state = { lat: null };
   }
 
   // React says we have to define Render!!!!
   render() {
-    this.state = { position: null };
-
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState(position),
       error => console.log(error)
