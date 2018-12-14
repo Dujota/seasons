940d42df713a99368578815437682e08c992fb40
Denis <ddujota@internationalsafety.com>
2018-12-13 21:21:40 -0500

    changed var mame and also toying around with a config object pattern

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 
+// Design pattern that is used very often, its called a config object at the top of the component
+const seasonConfig = {
+  summer: {},
+  winter: {}
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? 'Summer' : 'Winter';
+    return lat > 0 ? 'summer' : 'winter';
   } else {
-    return lat > 0 ? 'Winter' : 'Summer';
+    return lat > 0 ? 'winter' : 'summer';
   }
 };
 
 const SeasonDisplay = props => {
   const { lat } = props;
   const season = getSeason(lat, new Date().getMonth);
-  const text = season === 'Winter' ? "Brrr It's Chilly" : "Let's hit the beach";
-  const icon = season === 'Winter' ? 'snowflake' : 'sun';
+  const text = season === 'winter' ? "Brrr It's Chilly" : "Let's hit the beach";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return (
     <div>
